51e802ffe401e392b6824b433e3b7a4753fb81b1
Gabriel Lee <gabe.wblee@gmail.com>
2025-12-02 02:30:37 -0500

    Fixing left navbar issue

--- frontend/src/components/Profile/LeftNav.jsx
+++ frontend/src/components/Profile/LeftNav.jsx
@@ -37,29 +37,29 @@ function LeftMiddle({ endpoint }) {
         return null;
     }
     
-    const isHomeActive = matchPath({ path: "/profile/:utorid/home" }, endpoint);
+    const isHomeActive = matchPath({ path: "/home" }, endpoint);
     const homeTab = <div className={styles.leftNavHomeTab}>
-        <PageButton text="Home" active={isHomeActive} path={`/profile/${user.utorid}/home`}/>
+        <PageButton text="Home" active={isHomeActive} path={"/home"}/>
     </div>;
 
-    const isMyAccountActive = matchPath({ path: "/profile/:utorid/account" }, endpoint);
+    const isMyAccountActive = matchPath({ path: "/profile" }, endpoint);
     const myAccountTab = <div className={styles.leftNavMyAccountTab}>
-        <PageButton text="My Account" active={isMyAccountActive} path={`/profile/${user.utorid}/account`}/>
+        <PageButton text="My Account" active={isMyAccountActive} path={"/profile"}/>
     </div>;
 
-    const isTransferPointsActive = matchPath({ path: "/profile/:utorid/transfer-points" }, endpoint);
+    const isTransferPointsActive = matchPath({ path: "/transfer-points" }, endpoint);
     const transferPointsTab = <div className={styles.leftNavTransferPointsTab}>
-        <PageButton text="Transfer Points" active={isTransferPointsActive} path={`/profile/${user.utorid}/transfer-points`}/>
+        <PageButton text="Transfer Points" active={isTransferPointsActive} path={"/transfer-points"}/>
     </div>;
 
-    const isRedeemPointsActive = matchPath({ path: "/profile/:utorid/redeem-points" }, endpoint);
+    const isRedeemPointsActive = matchPath({ path: "/redeem-points" }, endpoint);
     const redeemPointsTab = <div className={styles.leftNavRedeemPointsTab}>
-        <PageButton text="Redeem Points" active={isRedeemPointsActive} path={`/profile/${user.utorid}/redeem-points`}/>
+        <PageButton text="Redeem Points" active={isRedeemPointsActive} path={"/redeem-points"}/>
     </div>;
 
-    const isPastTransactionsActive = matchPath({ path: "/profile/:utorid/past-transactions" }, endpoint);
+    const isPastTransactionsActive = matchPath({ path: "/past-transactions" }, endpoint);
     const pastTransactionsTab = <div className={styles.leftNavPastTransactionsTab}>
-        <PageButton text="Past Transactions" active={isPastTransactionsActive} path={`/profile/${user.utorid}/past-transactions`}/>
+        <PageButton text="Past Transactions" active={isPastTransactionsActive} path={"/past-transactions"}/>
     </div>;
 
     return <div className={styles.leftNavLeftMiddle}>
@@ -80,10 +80,10 @@ function LeftBottom() {
     </div>;
 }
 
-function LeftNav({ id, className }) {
+function LeftNav({ id }) {
     const location = useLocation();
     const endpoint = location.pathname.toLowerCase();
-    return <div id={id} className={`${styles.leftNav} ${className || ''}`}>
+    return <div id={id} className={styles.leftNav}>
         <div className={styles.leftNavScrollableContent}>
             <Menu />
             <LeftTop />
--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -127,7 +127,7 @@ export const AuthProvider = ({ children }) => {
             const userRes = await api.get("/users/me");
             setUser(userRes.data);
 
-            navigate(`/profile/${userRes.utorid}}/home`); // TODO: hypothetical "/home" page right now
+            navigate("/home"); // TODO: hypothetical "/home" page right now
             return null;
         } catch (err) {
             return "Network error"
